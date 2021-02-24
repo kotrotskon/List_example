c7f62885c4c3ce9b09d386644822fa908e08a4ce
KotrotsKon <kotrotskon@users.noreply.github.com>
2021-02-24 20:53:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/list_example/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.list_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.list);
+
+        String[] colors = {"Black", "Red", "Blue", "Green", "Yellow", "Orange", "Pink", "Brown"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, R.layout.item, colors);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                TextView txtVw = (TextView) view;
+                Log.d("ITEM_CLICK", txtVw.getText().toString());
+            }
+        });
+    }
+}
